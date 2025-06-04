e1fadfa3cd82127dab9aed54a3255cb55f74b7a9
Suhaibkhanmn <mnsuhaibkhan@gmail.com>
2025-06-04 22:02:31 +0530

    fixed frontend

--- project/components/TicketList.tsx
+++ project/components/TicketList.tsx
@@ -33,7 +33,7 @@ export function TicketList() {
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {getUserTickets?.map((ticketId: number) => (
+            {(getUserTickets ?? []).map((ticketId: number) => (
                 <Card key={ticketId}>
                     <CardHeader>
                         <CardTitle>Ticket #{ticketId}</CardTitle>
